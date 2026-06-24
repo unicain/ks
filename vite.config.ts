786ee845240cbe1547b8a6e2fed0b39edd0ca9d6
unicain <unicainteli@gmail.com>
2026-06-24 13:50:13 -0300

    Change base path from '/ks/' to './' in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ks/', // ⚠️ Certifique-se de que o nome aqui seja exatamente o nome do seu repositório no GitHub
+  base: './', // 🔥 Mudamos de '/ks/' para './' (caminho relativo universal)
 })
